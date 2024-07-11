680579e59f14d1f2b51ef4c900b9db1680898c98
kawadeajinkyak1 <ajinkyak192@gmail.com>
2024-07-11 22:13:21 +0530

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+/*
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Home from './Page/Home';
@@ -21,6 +22,37 @@ function App() {
   <Route path ='/Contact' element ={<Contact />} />
     </Routes>
     </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
+*/
+
+
+import {HashRouter, Routes, Route} from 'react-router-dom';
+import './App.css';
+import Home from './Page/Home';
+import Intro from './Components/Intro';
+import Abt from './Components/Abt';
+import Skills from './Components/Skills';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+<HashRouter>
+<Routes>
+  <Route path ='/' element ={<Home />} />
+  <Route path ='/Intro' element ={<Intro />} />
+  <Route path ='/About' element ={<Abt />} />
+  <Route path ='/Skills' element ={<Skills />} />
+  <Route path ='/Projects' element ={<Projects />} />
+  <Route path ='/Contact' element ={<Contact />} />
+    </Routes>
+    </HashRouter>
 
     </div>
   );
